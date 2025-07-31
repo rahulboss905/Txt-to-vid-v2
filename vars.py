50f3b7352b455a16864b1f2c7f1c5865cb847664
rahulboss905 <166490010+rahulboss905@users.noreply.github.com>
2025-07-31 11:58:28 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -1,13 +1,22 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#R̸a̸h̸u̸l̸
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
+API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
+API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
 OWNER = int(environ.get("OWNER", "7456681709"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
-LOG_CHANNEL = "-1002613759347" # log group me bot ko admin narur banaye full rights ke sath
+CREDIT = environ.get("CREDIT", "R̸a̸h̸u̸l̸")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7456681709').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '7456681709').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
